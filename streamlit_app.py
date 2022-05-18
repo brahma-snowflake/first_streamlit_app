83de10fb8e44ac151ea6d9a6d6af2258a771b282
Brahma <105260994+brahma-snowflake@users.noreply.github.com>
2022-05-17 22:18:17 -0500

    Update streamlit_app.py
    fixed error Fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 streamlit.header('Breakfast Favorites')
@@ -14,6 +14,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')  
-streamlit.multiselect("select ome fruits:",list(my_fruit_list.index))
+streamlit.multiselect("select some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
